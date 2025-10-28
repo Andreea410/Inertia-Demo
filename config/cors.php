1d4316f63dc022114f2af0b8a2d49bcd98f7cb20
Ungur Andreea <147054941+Andreea410@users.noreply.github.com>
2025-10-28 14:59:58 +0200

    Feat: Added CORS

---
+++ config/cors.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'allowed_methods' => ['*'],
+    'allowed_origins' => ['http://localhost:3000'],
+    'max_age' => 0,
+    'supports_credentials' => true,
+];
